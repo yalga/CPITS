d08f0af962b60c2a5c1e9927eea5eae0b9d651f9
yalga <59728440+yalga@users.noreply.github.com>
2021-06-29 08:40:50 -0500

    Add files via upload

--- cloudone_api.py
+++ cloudone_api.py
@@ -3,6 +3,9 @@ import sys, warnings
 import deepsecurity
 from deepsecurity.rest import ApiException
 from pprint import pprint
+import csv 
+import os
+from twilio.rest import Client
 
 # Setup
 if not sys.warnoptions:
@@ -28,3 +31,26 @@ try:
 except ApiException as e:
 	print("An exception occurred when calling ComputersApi.list_computers: %s\n" % e)
 
+sourcefile = open('api.csv', 'a')
+print(api_response, file = sourcefile)
+sourcefile.close()
+
+
+f = open (api.csv, 'w')
+f.write(api_response)
+f.close
+
+
+
+account_sid =  'AC61ab867f5cfd714a74d926dd7f075f7c'
+auth_token = '18a145f5d626bc98394b373e62ba470d'
+client = Client(account_sid, auth_token)
+
+message = client.api_response \
+                .create(
+                     body="This message is from Getachew Ferede. CPITS is amazing. Thank you Trend Micro",
+                     from_='+19704441341',
+                     to='+12023527923'
+                 )
+
+print(message.sid)
